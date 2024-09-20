57010e3a9ea56110a74b92fe2609d76c85a929cb
Souuppp <levmcg78@kingshigh.school.nz>
2024-09-20 13:03:55 +1200

    1.1.2 Bloodline
    Kill people without being seen!

--- Title_Screen.gd
+++ Title_Screen.gd
@@ -1,14 +1,10 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	if Input.is_action_pressed("select"):
-		load("res://scenes/world.tscn")
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/world2.tscn")
+	get_tree().change_scene_to_file("res://How to play.tscn")
 
 
 func _on_quit_pressed():
